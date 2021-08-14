13c6b2dc11be5c8a348f1ff084764157afde5c3c
Follmann, Jakob <jakob.follmann@haw-hamburg.de>
2021-08-14 23:40:10 +0200

    Add tmp log as console output

--- media_msg_bot.py
+++ media_msg_bot.py
@@ -1,5 +1,5 @@
-import discord
 from discord.ext import commands
+from datetime import datetime
 
 # setup discord bot
 client = commands.Bot(command_prefix='!')
@@ -22,22 +22,11 @@ async def on_message(msg):
 
     if msg.channel.id in channels:
         if 'media.discordapp.net' in msg.content and (msg.content.endswith('.mp4') or msg.content.endswith('.webm')):
+            print(f'{datetime.now().strftime("%Y-%m-%d/%H:%M:%S")}::{msg.author}::{msg.content}')
             new_msg = msg.content.replace('media.discordapp.net', 'cdn.discordapp.com')
             await msg.channel.send(new_msg)
             await msg.delete()
 
     await client.process_commands(msg)
-
-
-@client.command()
-async def archivpins(ctx, thread: discord.Thread):
-    channel = client.get_channel(ctx.channel.id)
-    pins = await channel.pins()
-    for msg in pins:
-
-        msgstr = '\n**%s**: [%s](%s)' % \
-                 (msg.author.name, msg.created_at.strftime('%H:%M %d-%B-%Y'), msg.jump_url)
-        await thread.send(embed=discord.Embed(description=msgstr, colour=0xFFFFFF))
-        await thread.send(msg.content)
 
 client.run('your discord token')
